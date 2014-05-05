5fa4004f281febe4e7eb5e52e086c006bde88684
watercosmos <watercosmos@gmail.com>
2014-05-05 19:43:20 +0800

    packet type
    add pack type template

--- main.c
+++ main.c
@@ -5,14 +5,31 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("lhy");
 MODULE_DESCRIPTION("test hook");
 
+int ip_test(struct sk_buff *skb,
+			struct net_device *dev,
+			struct packet_type *ptype,
+			struct net_device *dev2)
+{
+	return 0;
+}
+
+struct packet_type ptype = {
+	.type = __constant_htons(ETH_P_ALL);
+	.func = ip_test;
+};
+
+
 static int __init myhook_init(void)
 {
+	dev_add_pack(&ptype);
+
 	return 0;
 }
 
 static void __exit myhook_fini(void)
 {
-	
+	dev_remove_pack(&ptype);
+
 }
 
 module_init(myhook_init);
